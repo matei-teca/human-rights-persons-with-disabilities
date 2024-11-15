74cef8aa57b5df671fae2d451953da3a7f512469
Matei Teca <matei.teca@gmail.com>
2024-11-15 22:46:12 +0200

    fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,7 @@
-// src/components/Footer.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import './styles/Footer.css';
 
 function Footer() {
@@ -10,6 +11,7 @@ function Footer() {
         <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
         <nav>
           <ul className="footer-nav">
+            <li><Link to="/"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/thesis">Thesis</Link></li>
             <li><Link to="/contact">Contact</Link></li>
