05239cbe80c70523bc62844d62b023ac7fc483ff
Matei Teca <matei.teca@gmail.com>
2024-11-16 20:22:04 +0200

    fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <h1>Digital Accessibility & Well-Being</h1>
+        <h1>Digital Accessibility, Inclusive Human Rights & Well-Being</h1>
       </div>
       <nav className="nav">
         <ul>
